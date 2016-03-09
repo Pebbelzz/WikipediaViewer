66edd5f5d25992e3c04815f6e0a407eee54ffd0f
Pebbelzz <greeneyes14j@gmail.com>
2016-03-09 17:41:24 -0500

    Added retrieval of input search from DOM

--- main.js
+++ main.js
@@ -3,10 +3,11 @@ var random = "http://en.wikipedia.org/wiki/Special:Random"
 var inputSearch = "";
 
 function search(){
-  inputSearch = document.getElementById('wikiSearch').placeholder;
+  inputSearch =  $("#wikiSearch").val();
   xhr.open("GET", "https://en.wikipedia.org/w/api.php?action=query&titles=" + inputSearch + "&prop=info&format=jsonfm" );
   xhr.send();
   console.log(xhr);
+  console.log(inputSearch);
 }
 
 var testJSON = "https://en.wikipedia.org/w/api.php?action=query&titles=Albert%20Einstein&prop=info&format=jsonfm";
