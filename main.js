462203937c71e4f255e961d51d427500122c6ba1
Pebbelzz <greeneyes14j@gmail.com>
2016-03-09 22:19:19 -0500

    Worked with testing function for Wiki API

--- main.js
+++ main.js
@@ -1,19 +1,15 @@
-var xhr = new XMLHttpRequest();
-var random = "http://en.wikipedia.org/wiki/Special:Random"
-var inputSearch = "";
+var testJSON = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=Albert";
+var testList = {};
 
-function search(){
-  inputSearch =  $("#wikiSearch").val();
-  xhr.open("GET", "https://en.wikipedia.org/w/api.php?action=query&titles=" + inputSearch + "&prop=info&format=jsonfm" );
-  xhr.send();
-  console.log(xhr);
-  console.log(inputSearch);
-}
-
-var testJSON = "https://en.wikipedia.org/w/api.php?action=query&titles=Albert%20Einstein&prop=info&format=jsonfm";
-var otherTest = ""
 function testingAPI(){
-  xhr.open("GET", testJSON);
-  xhr.send();
-  console.log(xhr.status);
+  console.log(testList);
+  $.ajax({
+      url: testJSON,
+      dataType: "jsonp",
+      success: function(response){
+        testList = response[1];
+
+      }
+
+  })
 }
