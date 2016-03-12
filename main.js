80a3c05f052b418c53e7428563baf58cecab605a
Pebbelzz <greeneyes14j@gmail.com>
2016-03-12 18:19:05 -0500

    removed functions for static data that were no longer  in use

--- main.js
+++ main.js
@@ -1,57 +1,9 @@
-var apiURL = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=";
 var wikiArtTitle = "";
 var wikiArticleUrl = "";
 var wikiArtDesc = "";
 var searchTerm = "";
 var lastSearch = "";
 
-
-//static functions that return results once user presses enter or hits submit button
-
-function search(){
-  searchTerm = $("#wikiSearch").val();
-  getAPI();
-}
-
-function getAPI(){
-    $.ajax({
-      url: apiURL + searchTerm,
-      dataType: "jsonp",
-      success: function(response){
-        //console.log(searchTerm);
-        //console.log(apiURL);
-        wikiArtTitle = response[1];
-        //console.log(wikiArtTitle)
-        wikiArtDesc = response[2];
-        //console.log(wikiArtDesc);
-        wikiArticleUrl = response[3];
-        //console.log(wikiArticleUrl);
-        infoToPage();
-      }
-  })
-}
-function infoToPage(){
-    console.log("infoToPage() was called")
-    for ( i = 0; i < wikiArtTitle.length; i++ ){
-      if(lastSearch == searchTerm){
-        $("#results").append("<div id='articleInfo result[" + i + "]'><a href='" + wikiArticleUrl[i] + "' target='_blank'>" + wikiArtTitle[i] + "</a></div> \
-          " + wikiArtDesc[i]);
-        //console.log("searchTerm is the same as lastSearch and .append ran");
-        //console.log("lastSearch = searchTerm : " + lastSearch + " = " + searchTerm)
-      }
-      else{
-        //console.log("lastSearch doesn't  = searchTerm, .html ran")
-        //console.log("searchTerm: " + searchTerm)
-        //console.log("lastSearch: " + lastSearch)
-        $("#results").html("<div id='articleInfo result[" + i + "]'><a href='" + wikiArticleUrl[i] + "' target='_blank'>" + wikiArtTitle[i] + "</a></div> \
-          " + wikiArtDesc[i]);
-        //console.log("resetting lastSearch");
-        lastSearch = searchTerm;
-        //console.log("lastSearch: " + lastSearch)
-        //console.log("searchTerm: " + searchTerm)
-        }
-      }
-}
 
 
 //below are auto functions to add data as user types
