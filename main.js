f298dbbc865beb4646edcbee06facb8efc7ab682
Pebbelzz <greeneyes14j@gmail.com>
2016-03-12 18:38:04 -0500

    removed if statement for emptying #results
    If statement was unneeded due to each time there was a keystroke the
    search term was always going to be different from the last text in the
    search box

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
 var wikiArtTitle = "";
 var wikiArticleUrl = "";
 var wikiArtDesc = "";
-var searchTerm = "";
-var lastSearch = "";
-
-
 
 //below are auto functions to add data as user types
 
@@ -20,25 +16,18 @@ $(document).ready(function(){
     script.src = 'http://en.wikipedia.org/w/api.php?action=opensearch&limit=10&format=json&callback=autoSearch&search=' + textValue;
     $('body').append(script);
     console.log(script);
-    searchTerm = textValue;
-    console.log("searchTerm: " + searchTerm + " & lastSearch: " + lastSearch)
   });
 });
 
 function autoInfoToPage(){
   console.log("autoInfoToPage(); was called")
   console.log("wikiArtTitle.length: " + wikiArtTitle.length)
-  console.log("searchTerm: " + searchTerm + " & lastSearch: " + lastSearch)
-  if(searchTerm != lastSearch){
-    console.log("searchTerm !== lastSearch: " + searchTerm !== lastSearch)
-    $("#results").empty();
-  };
+  $("#results").empty();
   for ( i = 0; i < wikiArtTitle.length; i++ ){
     console.log("wikiArtTitle: " + wikiArtTitle);
     $("#results").append("<div id='articleInfo result[" + i + "]'><a href='" + wikiArticleUrl[i] + "' target='_blank'>" + wikiArtTitle[i] + "</a></div> \
       " + wikiArtDesc[i]);
   };
-  lastSearch = searchTerm;
 };
 
 function autoSearch(data){
